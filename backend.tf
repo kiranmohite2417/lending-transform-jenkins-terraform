9509a10f039984ca85e1548e6f45f229f760ae46
Kiran Mohite <kiranmohite2417@gmail.com>
2025-08-21 13:12:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "kiran-terraform-state-1"
+    bucket         = "kiran-terraform-state-11"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
